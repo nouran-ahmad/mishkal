1096ea41b54a3f897fe657b36eb93ac2387d001a
Nouran <nearat.nouran@gmail.com>
2018-10-05 14:26:58 +0000

    reverting back

--- interfaces/web/adawaty.py
+++ interfaces/web/adawaty.py
@@ -106,7 +106,7 @@ class webApp(baseWebApp):
     def help(self, rq, *args):
         return {
         }
-    @expose( headers=MyJsonHeaders)
+    @expose(jsonDumps, headers=MyJsonHeaders)
     def ajaxGet(self, rq,  *args):
         """
         this is an example of using ajax/json
